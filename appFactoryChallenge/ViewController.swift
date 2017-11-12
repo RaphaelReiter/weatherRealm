fd27b6d2fd572a19c44ec5d661d67a2723d4c0b5
Raphaël Reiter <rdso.reiter@gmail.com>
2017-11-12 15:55:24 +0100

    Initial Commit

---
+++ appFactoryChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  appFactoryChallenge
+//
+//  Created by Raphaël Reiter on 12/11/2017.
+//  Copyright © 2017 Raphaël Reiter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
