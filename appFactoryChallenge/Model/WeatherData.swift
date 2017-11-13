fc543e51d152f13427d5eb26c9753c15aca9f709
Raphaël Reiter <raphaelreiter@Raphaels-iMac.local>
2017-11-13 11:02:06 +0100

    project

---
+++ appFactoryChallenge/Model/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  appFactoryChallenge
+//
+//  Created by Raphaël Reiter on 12/11/2017.
+//  Copyright © 2017 Raphaël Reiter. All rights reserved.
+//
+
+import Foundation
---
+++ appFactoryChallenge/View/weatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  weatherCell.swift
+//  appFactoryChallenge
+//
+//  Created by Raphaël Reiter on 12/11/2017.
+//  Copyright © 2017 Raphaël Reiter. All rights reserved.
+//
+
+import UIKit
+
+class weatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
